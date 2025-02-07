fa8567148726eb357919f719464c4732fe41c851
Jan Supolik <jan.supolik@hotmail.com>
2025-02-07 11:14:21 +0100

    first commit

---
+++ app.ts
@@ -0,0 +1,56 @@
+function GetReview(title: string): string | number {
+    if (title === 'A New Hope') {
+        return 'Classic';
+    }
+    else if (title.length <= 5) {
+        return 'Ehhh ' + title;
+    }
+    return Math.floor(Math.random() * 6) + 1;    
+}
+
+let movieTitle: string = 'A New Hope';
+//movieTitle = 'The Force Awakens';
+//movieTitle = 'The ';
+
+let movieRating: string | number = GetReview(movieTitle);
+
+console.log(`Movie Title: ${movieTitle}`);
+
+if (typeof movieRating === 'string') {
+    console.log(`Review: ${movieRating}`);
+}
+else {
+    console.log(`Rating: ${movieRating}/10`);
+}
+
+// for (let i = 1; i <=10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`Even number: ${i}`);
+//   }
+//   else {
+//     console.log(`Odd number: ${i}`);
+//   }
+// }
+
+// let i:number = 1;
+// while (i <= 10) {
+//   if (i % 2 === 0) {
+//     console.log(`Even number: ${i}`);
+//   }
+//   else {
+//     console.log(`Odd number: ${i}`);
+//   }
+//   i++;
+// }
+
+// let fruit: string = 'apple';
+// switch (fruit) {
+//     case 'apple' :
+//         console.log('Apple');
+//         break;
+//     case 'banana':
+//         console.log('Banana');
+//         break;
+//     default: 
+//     break
+// }
