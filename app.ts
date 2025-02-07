94af9c43dd55486b2debd3d9fc4aaaa2938f5124
Jan Supolik <jan.supolik@hotmail.com>
2025-02-07 16:21:34 +0100

    Refactor GetReview function and related logic to comments; add LogMessage function for logging

--- app.ts
+++ app.ts
@@ -1,27 +1,27 @@
-function GetReview(title: string): string | number {
-    if (title === 'A New Hope') {
-        return 'Classic';
-    }
-    else if (title.length <= 5) {
-        return 'Ehhh ' + title;
-    }
-    return Math.floor(Math.random() * 10);    
-}
+// function GetReview(title: string): string | number {
+//     if (title === 'A New Hope') {
+//         return 'Classic';
+//     }
+//     else if (title.length <= 5) {
+//         return 'Ehhh ' + title;
+//     }
+//     return Math.floor(Math.random() * 10);
+// }
 
-let movieTitle: string = 'A New Hope';
-movieTitle = 'The Force Awakens';
-//movieTitle = 'The ';
+// let movieTitle: string = 'A New Hope';
+// movieTitle = 'The Force Awakens';
+// //movieTitle = 'The ';
 
-let movieRating: string | number = GetReview(movieTitle);
+// let movieRating: string | number = GetReview(movieTitle);
 
-console.log(`Movie Title: ${movieTitle}`);
+// console.log(`Movie Title: ${movieTitle}`);
 
-if (typeof movieRating === 'string') {
-    console.log(`Review: ${movieRating}`);
-}
-else {
-    console.log(`Rating: ${movieRating}/10`);
-}
+// if (typeof movieRating === 'string') {
+//     console.log(`Review: ${movieRating}`);
+// }
+// else {
+//     console.log(`Rating: ${movieRating}/10`);
+// }
 
 let adder = (a: number, b: number): number => a + b;
 var sum = adder(5, 10);
@@ -36,5 +36,11 @@ highScores = scores.filter((element, index, array) => {
     }
     return false;
 });
+
+function LogMessage(message: string): void {
+    console.log(message);
+}
+
+LogMessage("Hello World");
 
 console.log("end");
--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 "use strict";
-function GetReview(title) {
-    if (title === 'A New Hope') {
-        return 'Classic';
-    }
-    else if (title.length <= 5) {
-        return 'Ehhh ' + title;
-    }
-    return Math.floor(Math.random() * 10);
-}
-let movieTitle = 'A New Hope';
-movieTitle = 'The Force Awakens';
-//movieTitle = 'The ';
-let movieRating = GetReview(movieTitle);
-console.log(`Movie Title: ${movieTitle}`);
-if (typeof movieRating === 'string') {
-    console.log(`Review: ${movieRating}`);
-}
-else {
-    console.log(`Rating: ${movieRating}/10`);
-}
+// function GetReview(title: string): string | number {
+//     if (title === 'A New Hope') {
+//         return 'Classic';
+//     }
+//     else if (title.length <= 5) {
+//         return 'Ehhh ' + title;
+//     }
+//     return Math.floor(Math.random() * 10);
+// }
+// let movieTitle: string = 'A New Hope';
+// movieTitle = 'The Force Awakens';
+// //movieTitle = 'The ';
+// let movieRating: string | number = GetReview(movieTitle);
+// console.log(`Movie Title: ${movieTitle}`);
+// if (typeof movieRating === 'string') {
+//     console.log(`Review: ${movieRating}`);
+// }
+// else {
+//     console.log(`Rating: ${movieRating}/10`);
+// }
 let adder = (a, b) => a + b;
 var sum = adder(5, 10);
 console.log(sum);
@@ -31,4 +31,8 @@ highScores = scores.filter((element, index, array) => {
     }
     return false;
 });
+function LogMessage(message) {
+    console.log(message);
+}
+LogMessage("Hello World");
 console.log("end");
